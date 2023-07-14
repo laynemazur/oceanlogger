d86019b76070438eaaae62fb720c9f83e9a8fecb
David Huynh <133894052+DaviHynh@users.noreply.github.com>
2023-07-13 20:31:25 -0400

    Fixed login api call in Backend.dart

--- lib/database/Backend.dart
+++ lib/database/Backend.dart
@@ -3,20 +3,43 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class ConnectBackend  {
-  //static var response;
+  // The prints in this file are for debugging, uncomment to see whats going on.
 
   static Future<String> authenticateUser(String login, String password) async {
-    String ret = '';
 
-    var url = Uri.https('oceanlogger-046c28329f84.herokuapp.com', 'api/login');
-    //String response = await http.get(url,  headers: {'login': 'laymazur', 'password': 'Laynie@2020'});
+    // Create a URL to the API you want to access.
+    var url = Uri.https("oceanlogger-046c28329f84.herokuapp.com", "api/login");
 
-    http.Response result = await http.post(url, body: utf8.encode('login: laymazur, password: Laynie@2020'),
-      headers: { "Accept": "application/json", "Content-Type": "application/json",}, encoding: Encoding.getByName("utf-8"));
+    // print(url);
 
-    ret = result.body;
+    // Header for the POST call.
+    Map<String,String> headers = {'Content-Type': 'application/json'};
 
-    return ret;
+    // Body for the POST call.
+    final jsonObject = jsonEncode(
+      {
+        "login":"testuser1",
+        "password":"Orange123!"
+      }
+    );
+
+    // print(jsonObject);
+
+    // Make the API call.
+    var response = await http.post(url, headers:headers,body:jsonObject);
+
+    // print(response.statusCode);
+    // print(response.body);
+
+    // Parse the returned body.
+    final retJsonObject = jsonDecode(response.body);
+
+    // print(retJsonObject);
+
+    // Returns an empty string for no error, or an error message string.
+    String err = retJsonObject['error'];
+
+    return err;
   }
 
 
