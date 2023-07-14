01a6d33b73fa43eb853349fd61e0dbe19c58dc4c
David Huynh <133894052+DaviHynh@users.noreply.github.com>
2023-07-13 20:35:09 -0400

    Fixed logic in LoginButton.dart
    The "Login.authUser()" function returns an empty string or a string with an error message, so I just made the if statement check that.

--- lib/buttons/LoginButton.dart
+++ lib/buttons/LoginButton.dart
@@ -10,14 +10,9 @@ class LoginButton extends StatelessWidget {
       child: ElevatedButton(
         child: Text('Login'),
         onPressed: () async {
-          String retError = "";
           String ret = await Login.authUser();
-          var jsonObject = json.decode(ret);
-          print(jsonObject);
 
-          retError = jsonObject["error"];
-
-          if (retError == "") {
+          if (ret == "") {
             Navigator.push(
               context,
               MaterialPageRoute(builder: (context) => HomePage()),
@@ -27,7 +22,7 @@ class LoginButton extends StatelessWidget {
               context: context,
               builder: (context) => AlertDialog(
                 title: Text('Error'),
-                content: Text(retError),
+                content: Text(ret),
                 actions: [
                   TextButton(
                     child: Text('OK'),
