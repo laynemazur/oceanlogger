e01afab555dfda676eafbea7d8ed74a80c0b1a8c
ddelacruz42 <91350250+ddelacruz42@users.noreply.github.com>
2023-07-28 18:16:37 -0700

    updated styling of register

--- lib/components/Register.dart
+++ lib/components/Register.dart
@@ -16,52 +16,53 @@ class Register extends StatelessWidget{
       children: <Widget>[
         Container(
           //login design/textbox
-          padding: const EdgeInsets.all(10),
+          margin: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
           decoration: const BoxDecoration(
-              border: Border(
-                bottom: BorderSide(color: Colors.grey),
-              )
+
           ),
           child: TextField(
             controller: emailController,
             decoration: InputDecoration(
                 hintText: "email",
                 hintStyle: TextStyle(color: Colors.grey),
-                border: InputBorder.none
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                )
             ),
           ),
         ),
         Container(
           //login design/textbox
-          padding: const EdgeInsets.all(10),
+          margin: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
           decoration: const BoxDecoration(
-              border: Border(
-                bottom: BorderSide(color: Colors.grey),
-              )
+
           ),
           child: TextField(
             controller: loginController,
             decoration: InputDecoration(
                 hintText: "username",
                 hintStyle: TextStyle(color: Colors.grey),
-                border: InputBorder.none
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                )
             ),
           ),
         ),
         Container(
           //password design/textbox
-          padding: const EdgeInsets.all(10),
+          margin: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
           decoration: const BoxDecoration(
-              border: Border(
-                  bottom: BorderSide(color: Colors.grey)
-              )
+
           ),
           child: TextField(
             controller: passwordController,
+            obscureText: true,
             decoration: InputDecoration(
                 hintText: "password",
                 hintStyle: TextStyle(color: Colors.grey),
-                border: InputBorder.none
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                )
             ),
           ),
         ),
