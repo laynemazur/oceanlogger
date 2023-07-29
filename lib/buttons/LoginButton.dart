e0ac3c316d2349f3d112fc6aad1aa4a7bbef01d0
ddelacruz42 <91350250+ddelacruz42@users.noreply.github.com>
2023-07-28 18:20:20 -0700

    updated styling of loginbutton

--- lib/buttons/LoginButton.dart
+++ lib/buttons/LoginButton.dart
@@ -7,34 +7,43 @@ class LoginButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: ElevatedButton(
-        child: Text('Login'),
-        onPressed: () async {
-          String ret = await Login.authUser();
+      child: SizedBox(
+        width: 330,
+        height: 45,
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              )
+          ),
+          child: Text('Login'),
+          onPressed: () async {
+            String ret = await Login.authUser();
 
-          if (ret == "") {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => HomePage()),
-            );
-          } else {
-            showDialog(
-              context: context,
-              builder: (context) => AlertDialog(
-                title: Text('Error'),
-                content: Text(ret),
-                actions: [
-                  TextButton(
-                    child: Text('OK'),
-                    onPressed: () {
-                      Navigator.of(context).pop(); // Close the dialog
-                    },
-                  ),
-                ],
-              ),
-            );
-          }
-        },
+            if (ret == "") {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => HomePage()),
+              );
+            } else {
+              showDialog(
+                context: context,
+                builder: (context) => AlertDialog(
+                  title: Text('Error'),
+                  content: Text(ret),
+                  actions: [
+                    TextButton(
+                      child: Text('OK'),
+                      onPressed: () {
+                        Navigator.of(context).pop(); // Close the dialog
+                      },
+                    ),
+                  ],
+                ),
+              );
+            }
+          },
+        ),
       ),
     );
   }
