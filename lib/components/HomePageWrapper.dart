c7192c89f7b017efc3e092769de8847db7db4e59
ddelacruz42 <91350250+ddelacruz42@users.noreply.github.com>
2023-07-29 09:19:50 -0700

    updated styling/add bottom nav bar

--- lib/components/HomePageWrapper.dart
+++ lib/components/HomePageWrapper.dart
@@ -1,23 +1,69 @@
 import 'package:flutter/material.dart';
-import '../buttons/AddLogButton.dart';
-import '../buttons/DiveCalcButton.dart';
+import 'package:oceanlogger/buttons/DiveCalcButton.dart';
+import '../pages/AddLogPage.dart';
+import '../pages/DiveCalcPage.dart';
 
-//Design for LoginPage, calls Login which performs actions on this page
-class HomePageWrapper extends StatelessWidget {
-  const HomePageWrapper({super.key});
+class HomePageWrapper extends StatefulWidget {
+  const HomePageWrapper({Key? key}) : super(key: key);
+
+  @override
+  State<HomePageWrapper> createState() => _HomePageWrapper();
+}
+
+class _HomePageWrapper extends State<HomePageWrapper> {
+  int _selectedIndex = 1;
+
+  static const List<Widget> _pages = <Widget>[
+    AddLogPage(),
+    // where list of logs will be
+    Icon(
+      Icons.home,
+      size: 150,
+    ),
+    DiveCalcPage(),
+    // Icon(
+    //   Icons.chat,
+    //   size: 150,
+    // ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            const SizedBox(height: 40,),
-            AddLogButton(),
-            const SizedBox(height: 40,),
-            DiveCalcButton()
-          ],
-        ),
+      //appBar: AppBar(
+      // has a dandy back button built in, takes up space though
+      //title: const Text('ScubaLogger'),
+      //),
+      body: Center(
+        child: _pages.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            label: 'Add Log',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calculate),
+            label: 'Dive Calculator',
+          ),
+          // BottomNavigationBarItem(
+          //   icon: Icon(Icons.chat),
+          //   label: 'Chats',
+          // ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
       ),
     );
   }
