d3210fe03aba3251719b00e8f0ba1c68e9b7ccd4
ddelacruz42 <91350250+ddelacruz42@users.noreply.github.com>
2023-07-28 18:16:08 -0700

    updated styling of registerwrapper

--- lib/components/RegisterWrapper.dart
+++ lib/components/RegisterWrapper.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import '../buttons/RegisterButton.dart';
 import '../buttons/SignInButton.dart';
 import 'Register.dart';
+import '../pages/LoginPage.dart';
 
 //Design for RegisterPage, calls Register which performs actions on this page
 class RegisterWrapper extends StatelessWidget {
@@ -15,6 +16,45 @@ class RegisterWrapper extends StatelessWidget {
         // Scaffold keeps keyboard popup on emulator from overflowing DO NOT DELETE
         child: Column(
           children: <Widget>[
+            Container(
+              child: Image.asset("assets/images/006-shark-1.png",
+                  width: 100 ),
+            ),
+            Row(
+              children: <Widget>[
+                Container(
+                  alignment: Alignment.centerLeft,
+                  padding: const EdgeInsets.symmetric(horizontal: 40),
+                  child: const Text(
+                    "Register",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                        fontSize: 40),
+                    textAlign: TextAlign.left,
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.centerLeft,
+                  padding: const EdgeInsets.symmetric(horizontal: 40),
+                  child: GestureDetector(
+                    onTap: () => {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => LoginPage()))
+                    },
+                    child: const Text(
+                      "Have An Account?\n Sign in!",
+                      style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey),
+                    ),
+                  ),
+                ),
+              ],
+            ),
             const SizedBox(height: 40,),
             Container(
               decoration: BoxDecoration(
@@ -25,8 +65,8 @@ class RegisterWrapper extends StatelessWidget {
             ),
             const SizedBox(height: 40,),
             RegisterButton(),
-            const SizedBox(height: 40,),
-            SignInButton()
+            //const SizedBox(height: 40,),
+            //SignInButton()
           ],
         ),
       ),
