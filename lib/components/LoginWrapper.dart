a2b1b723fce4b6bb8a10e4911440e0bee5166b6f
ddelacruz42 <91350250+ddelacruz42@users.noreply.github.com>
2023-07-28 18:16:56 -0700

    updated styling of loginwrapper

--- lib/components/LoginWrapper.dart
+++ lib/components/LoginWrapper.dart
@@ -3,6 +3,7 @@ import '../buttons/LoginButton.dart';
 import '../buttons/SignUpButton.dart';
 import '../buttons/ForgotButton.dart';
 import 'Login.dart';
+import '../pages/RegisterPage.dart';
 
 //Design for LoginPage, calls Login which performs actions on this page
 class LoginWrapper extends StatelessWidget {
@@ -16,6 +17,45 @@ class LoginWrapper extends StatelessWidget {
         // Scaffold keeps keyboard popup on emulator from overflowing DO NOT DELETE
         child: Column(
           children: <Widget>[
+            Container(
+              child: Image.asset("assets/images/006-shark-1.png",
+                  width: 100 ),
+            ),
+            Row(
+              children: <Widget>[
+                Container(
+                  alignment: Alignment.centerLeft,
+                  padding: const EdgeInsets.symmetric(horizontal: 40),
+                  child: const Text(
+                    "Login",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                        fontSize: 40),
+                    textAlign: TextAlign.left,
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.centerLeft,
+                  padding: const EdgeInsets.symmetric(horizontal: 40),
+                  child: GestureDetector(
+                    onTap: () => {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => RegisterPage()))
+                    },
+                    child: const Text(
+                      "No Account?\n Sign up",
+                      style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey),
+                    ),
+                  ),
+                ),
+              ],
+            ),
             const SizedBox(height: 40,),
             Container(
               decoration: BoxDecoration(
@@ -26,10 +66,10 @@ class LoginWrapper extends StatelessWidget {
             ),
             const SizedBox(height: 40,),
             LoginButton(),
-            const SizedBox(height: 40,),
-            SignUpButton(),
-            const SizedBox(height: 40,),
-            ForgotButton()
+            //const SizedBox(height: 40,),
+            //SignUpButton(),
+            //const SizedBox(height: 40,),
+            //ForgotButton()
           ],
         ),
       ),
