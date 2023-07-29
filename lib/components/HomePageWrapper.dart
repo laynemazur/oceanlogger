960dc41932d4fb9b26cfc44b107bc856b90dcb72
David Huynh <133894052+DaviHynh@users.noreply.github.com>
2023-07-29 12:55:47 -0400

    Added log display in HomePageWrapper.dart

--- lib/components/HomePageWrapper.dart
+++ lib/components/HomePageWrapper.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:oceanlogger/buttons/DiveCalcButton.dart';
 import '../pages/AddLogPage.dart';
 import '../pages/DiveCalcPage.dart';
+import '../database/Backend.dart';
+import '../pages/ViewLogPage.dart';
 
 class HomePageWrapper extends StatefulWidget {
   const HomePageWrapper({Key? key}) : super(key: key);
@@ -36,12 +38,22 @@ class _HomePageWrapper extends State<HomePageWrapper> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // APPBAR CAN WORK AS THE TOP PART FOR HOMEPAGE PROBABLY
       //appBar: AppBar(
       // has a dandy back button built in, takes up space though
       //title: const Text('ScubaLogger'),
       //),
       body: Center(
-        child: _pages.elementAt(_selectedIndex),
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              // ADD A WIDGET TO SEARCH THROUGH THE LOGS HERE!
+
+              // THIS DISPLAYS ALL OF THE LOGS.
+              DisplayData()
+            ],
+          ),
+        ),
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
@@ -64,6 +76,130 @@ class _HomePageWrapper extends State<HomePageWrapper> {
         ],
         currentIndex: _selectedIndex,
         onTap: _onItemTapped,
+      ),
+    );
+  }
+}
+
+
+
+// Call DisplayData() to display all the logs.
+class DisplayData extends StatefulWidget {
+  const DisplayData({super.key});
+
+  @override
+  State<DisplayData> createState() => _DisplayData();
+}
+
+class _DisplayData extends State<DisplayData> {
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTextStyle(
+      // style: Theme.of(context).textTheme.displaySmall!,
+      style: const TextStyle(color: Colors.black),
+      textAlign: TextAlign.center,
+      child: FutureBuilder<dynamic>(
+        future: ConnectBackend.searchDiveLog("", "", "", ""),
+        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+          List<Widget> children;
+          if (snapshot.hasData) {
+            children = <Widget>[
+              ListView.builder(
+                physics: const NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                itemCount: snapshot.data.length,
+                itemBuilder: (context, index){
+                  return InkWell(
+                      splashFactory: InkRipple.splashFactory,
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => ViewLogPage(data: snapshot.data[index])),
+                        );
+                      }, // Handle your callback
+                      child: Table(
+                        border: TableBorder.symmetric(outside: BorderSide(color: Colors.black)),
+                        children: [
+                          TableRow(
+                              children: [
+                                // Modify Title Here.
+                                Container(
+                                  child: Text(snapshot.data[index]["title"], textAlign: TextAlign.left, style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 16
+                                  ),),
+                                  padding: EdgeInsets.all(16.0),
+                                ),
+                                // Modify Date Here.
+                                Container(
+                                  child: Text(snapshot.data[index]["date"], textAlign: TextAlign.right, style: TextStyle(
+                                      color: Color(0xFFa1a1a1)
+                                  ),),
+                                  padding: EdgeInsets.all(16.0),
+                                ),
+                              ]
+                          ),
+                          TableRow(
+                              children: [
+                                Container(
+                                  // Modify Location Here.
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                        color: Colors.blue,
+                                        borderRadius: BorderRadius.all(Radius.circular(9.5))
+                                    ),
+                                    width: 85,
+                                    child: Text(snapshot.data[index]["location"], style: TextStyle(color: Colors.white),),
+                                    padding: EdgeInsets.all(3.5),
+                                  ),
+                                  padding: EdgeInsets.all(16),
+                                  alignment: Alignment.centerLeft,
+                                ),
+                                Container(
+                                  child: Text(""),
+                                  padding: EdgeInsets.all(12.0),
+                                ),
+                              ]
+                          ),
+                        ],
+                      )
+                  );
+                },
+              )
+            ];
+          } else if (snapshot.hasError) {
+            children = <Widget>[
+              const Icon(
+                Icons.error_outline,
+                color: Colors.red,
+                size: 60,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 16),
+                child: Text('Error: ${snapshot.error}'),
+              ),
+            ];
+          } else {
+            children = const <Widget>[
+              SizedBox(
+                width: 60,
+                height: 60,
+                child: CircularProgressIndicator(),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 16),
+                child: Text('Awaiting data...'),
+              ),
+            ];
+          }
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: children,
+            ),
+          );
+        },
       ),
     );
   }
